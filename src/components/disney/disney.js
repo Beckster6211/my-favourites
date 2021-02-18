1772769279040fb5fbd59baa1ad7d1ce10340856
= <beckyljones151192@gmail.com>
2021-02-18 19:03:13 +0000

    disney on another page

--- src/components/disney/disney.js
+++ src/components/disney/disney.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from "react";
 
-function DisneyViewer({ disneyId }) {
+function DisneyViewer() {
   const [disney, setDisney] = useState({});
+  const [mickeyId, setMickeyId] = useState();
 
-  //   useEffect(() => {
-  //     if (disney) {
-  async function getDisney() {
-    const res = await fetch(
-      `https://api.disneyapi.dev/characters/mickey mouse`,
-      {
-        headers: { accept: "application/json" },
-      }
-    );
-    const data = await res.json();
-    console.log(data);
-    setDisney(data);
-    // console.log(data.sprites.front_default);
-    // console.log(disney);
+  function handleClickDisney() {
+    setMickeyId("mickeymouse");
   }
-  getDisney();
-  //     }
-  //   }, [disneyId]);
+
+  useEffect(() => {
+    if (disney) {
+      async function getDisney() {
+        const res = await fetch(`https://api.disneyapi.dev/characters.json`, {
+          headers: { accept: "application/json" },
+        });
+        const data = await res.json();
+        console.log(data);
+        setDisney(data);
+        // console.log(data.sprites.front_default);
+        // console.log(disney);
+      }
+      getDisney();
+    }
+  }, [mickeyId]);
 
   return (
     <div className="disney-viewer">
+      <h1>Disney</h1>
+      <button onClick={handleClickDisney}>
+        My favourite Disney Character is...
+      </button>
       {/* <p>{pokemon?.species?.name}</p>
       <img
         //alt="pokemon"
